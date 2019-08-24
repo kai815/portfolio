ee3fb0abbfe29db8319bc2b3d876cfc4c1c8247c
kai815 <h.kaizuka815@gmail.com>
2019-08-24 13:01:24 +0900

    create about.vue and modify index.vue

---
+++ nuxt-project/pages/about.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="contents">
+    <section class="sec-title">
+      <h1 class="title">
+        About
+      </h1>
+      <h2>
+        My skills and personality
+      </h2>
+    </section>
+    <section class="sec-contents">
+      <h2 class="contents-title">
+        Skills
+      </h2>
+      <h2 class="contents-title">
+        Personality
+      </h2>
+    </section>
+  </div>
+</template>
+<style>
+.contents {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: block;
+  justify-content: center;
+  text-align: center;
+}
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #000000;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #000000;
+  word-spacing: 5px;
+  padding-top: 15px;
+  padding-bottom: 70px;
+}
+
+.sec-title {
+  background-color:#D3D3D3;
+  padding: 50px;
+  width: 100%;
+}
+
+.sec-contents {
+  background-color:white;
+  width: 100%;
+}
+.contents-title {
+  text-align: left;
+  padding: 10px 0px 10px 50px;
+}
+</style>

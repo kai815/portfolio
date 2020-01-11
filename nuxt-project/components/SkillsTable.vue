9393f4689d0fe2d981c9b06a6e0fd1d6ac147881
kai815 <h.kaizuka815@gmail.com>
2020-01-11 16:04:28 +0900

    add-css-adminSkillPage

--- nuxt-project/components/SkillsTable.vue
+++ nuxt-project/components/SkillsTable.vue
@@ -1,28 +1,57 @@
 <template>
   <tr>
-    <td>
+    <td class="admin-skill-contents__td admin-skill-contents__td_nowrap">
       {{ skill.number }}
     </td>
-    <td v-if="!isEditableName" @dblclick="isEditableName = true">
+    <td class="admin-skill-contents__td admin-skill-contents__td_nowrap" v-if="!isEditableName" @dblclick="isEditableName = true">
       {{ skill.name }}
     </td>
     <td v-else>
-      <input :value="skill.name" type="text" @blur="updateName($event.target.value, skill.id)">
+      <input class="admin-skill-contents__td__input" :value="skill.name" type="text" @blur="updateName($event.target.value, skill.id)">
     </td>
-    <td v-if="!isEditableDisc" @dblclick="isEditableDisc = true">
+    <td class="admin-skill-contents__td" v-if="!isEditableDisc" @dblclick="isEditableDisc = true">
       {{ skill.discription }}
     </td>
     <td v-else>
-      <input :value="skill.discription" type="text" @blur="updateDisc($event.target.value, skill.id)">
+      <textarea class="admin-skill-contents__td__input" :value="skill.discription" type="text" @blur="updateDisc($event.target.value, skill.id)"></textarea>
     </td>
-    <td>
-      <button @click="remove(skill.id)">
-        削除
+    <td class="admin-skill-contents__td">
+      <button class="delete-button" @click="remove(skill.id)">
+        <font-awesome-icon class="fa-1x fa-fw" :icon="['fas', 'times']"/>
       </button>
     </td>
   </tr>
 </template>
-
+<style>
+textarea {
+  resize: none;
+}
+.admin-skill-contents__td{
+  color: #000000;
+  border-bottom:0.5px solid #ccc;
+  background-color: #fafcfc;
+  border-radius: 1px;
+  padding: 5px;
+}
+.admin-skill-contents__td_nowrap {
+  white-space: nowrap;
+}
+.admin-skill-contents__td__input{
+  width: 100%;
+  padding:5px;
+  box-sizing:border-box;
+}
+.delete-button{
+  background-color: rgb(91, 91, 91);
+  color: #FFFFFF;
+  border-radius: 50%;
+}
+.delete-button:hover{
+  background-color: rgb(235, 11, 11);
+  color: #FFFFFF;
+  border-radius: 50%;
+}
+</style>
 <script>
 export default {
   props: {
